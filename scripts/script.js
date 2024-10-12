695d81f53e8de304cef9767b146c62e63b578cb9
Cecilia <ceciliafeng0@gmail.com>
2024-10-11 20:15:59 -0700

    hawk tuah spit on that thang

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,13 @@ function generateNavigationBar() {
         makeNavigationInfo("index", "Home"),
         makeNavigationInfo("programming", "Programming stuff")
     ];
-    let page = window.location.pathname.split("/").pop();
+    let page = window.location.pathname;
+    if (page.startsWith("/"))
+        page = page.substring(1, page.length);
     if (page.endsWith(".html"))
         page = page.substring(0, page.length - 5);
+    if (page.length === 0)
+        page = "index";
     let navElement = document.getElementById("navigation");
     for (const navigationInfo of navigationLinks) {
         const element = document.createElement("div");
